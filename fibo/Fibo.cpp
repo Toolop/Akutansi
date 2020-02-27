4d0f44062acab91d82ebbf853c63cdb664d40db7
Toolop <toolop2312@gmail.com>
2020-02-27 19:31:18 +0700

    first commit

---
+++ fibo/Fibo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<exception>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int range,n1,n2,n3;
+    cout<<"Enter a number : ";
+    cin>>range;
+    n1 = 0;
+    n2 = 1;
+    n3 = 1;
+    for (int i = 0; i < range; i++){
+       cout<<n3<<endl;
+       n3 = n1 + n2;
+       n1 = n2;
+       n2 = n3;
+    }
+  return 0;
+}
